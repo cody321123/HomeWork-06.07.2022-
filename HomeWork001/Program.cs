af031b42bce97e304372d20382f8572af25cf7b4
«Aleksandr» <navalniu202@gmail.com>
2022-07-06 13:54:06 +0300

    HomeWork001

---
+++ HomeWork001/Program.cs
@@ -0,0 +1,34 @@
+﻿void RandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(1,100);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+}
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+RandomNumbers(numbers);
+Console.Write("Вот наш массив: " );
+PrintArray(numbers);
+int count = 0;
+
+for (int z = 0; z < numbers.Length; z++)
+if (numbers[z] % 2 == 0)
+count++;
+
+Console.WriteLine("\tЧетных чисел = " +  count);
+
+
+
+
+
